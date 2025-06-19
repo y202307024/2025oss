3ee512069ec2a816c0ca7d61ebe5268b25e0eaa9
202307024 <audrud15@yuhan.ac.kr>
2025-06-19 10:46:11 +0900

    [+] calc 파일에 -999 넘어가면 멈춤 코드 추가

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -19,8 +19,13 @@ int main()
 	{
 		std::cout << "첫번째 수를 입력하세요 : ";
 		std::cin >> x;
+		if (-999 == x) {
+			printf("프로그램을 종료합니다\n");
+			break;
+		}
 		std::cout << "두번째 수를 입력하세요 : ";
 		std::cin >> y;
+
 
 		printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
 		printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
